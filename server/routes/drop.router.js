5c4b651d5565b9631c2444290b3c825574687684
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2021-02-02 19:21:59 -0600

    GET gender

---
+++ server/routes/drop.router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const pool = require("../modules/pool");
+const router = express.Router();
+
+// GET gender
+router.get("/gender", (req, res) => {
+  const queryText = "SELECT * FROM gender";
+  pool
+    .query(queryText)
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.log("GET gender failed: ", err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
