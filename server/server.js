14804facf2488c1e1d887680a8b80163064a2699
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2021-02-04 09:34:37 -0600

    POST router for matches

--- server/server.js
+++ server/server.js
@@ -8,12 +8,13 @@ const sessionMiddleware = require("./modules/session-middleware");
 const passport = require("./strategies/user.strategy");
 
 // Route includes
-const categoryRouter = require('./routes/category.router');
-const matchRouter = require('./routes/match.router');
-const acctType = require('./routes/acctType.router');
+const categoryRouter = require("./routes/category.router");
+const matchRouter = require("./routes/match.router");
+const acctType = require("./routes/acctType.router");
 const userRouter = require("./routes/user.router");
 const dropRouter = require("./routes/drop.router");
-const vetRouter = require('./routes/vet.router');
+const vetRouter = require("./routes/vet.router");
+const joinRouter = require("./routes/join-org.router.js");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -27,13 +28,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Routes */
-app.use('/api/user', userRouter);
-app.use('/api/category', categoryRouter);
-app.use('/api/match', matchRouter);
-app.use('/api/vet', vetRouter);
-app.use('/api/acctType', acctType);
+app.use("/api/user", userRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/match", matchRouter);
+app.use("/api/vet", vetRouter);
+app.use("/api/acctType", acctType);
 app.use("/api/user", userRouter);
 app.use("/api/drop", dropRouter);
+app.use("/api/join", joinRouter);
 
 // Serve static files
 app.use(express.static("build"));
