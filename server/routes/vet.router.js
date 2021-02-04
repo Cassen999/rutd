23bf1f9d3ea3e9a12b5260220df09a138b10a6d8
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2021-02-04 09:24:16 -0600

    GET all veterans

--- server/routes/vet.router.js
+++ server/routes/vet.router.js
@@ -1,23 +1,38 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 // GETs all vets by name limited to 10
 // This sqlText will have to change once there is info in vet table
 // Include a limit and order by
 
-router.get('/', (req, res) => {
-    const sqlText = `SELECT "user".username FROM "user" 
+router.get("/", (req, res) => {
+  const sqlText = `SELECT "user".username FROM "user" 
                     WHERE "user".type_id = 1;`;
-    pool.query(sqlText)
+  pool
+    .query(sqlText)
     .then((result) => {
-        console.log('Getting vets by name result', result.rows)
-        res.send(result.rows)
+      console.log("Getting vets by name result", result.rows);
+      res.send(result.rows);
     })
     .catch((error) => {
-        console.log('ERROR in vet GET', error)
-        res.sendStatus(500)
-    })
+      console.log("ERROR in vet GET", error);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * GET all veterans
+ */
+router.get("/", (req, res) => {
+  const queryText = "SELECT * FROM veteran;";
+  pool
+    .query(queryText)
+    .then((data) => res.json(data.rows))
+    .catch((err) => {
+      console.log("GET Veteran failed: ", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
