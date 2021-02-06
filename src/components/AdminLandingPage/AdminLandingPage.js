b8cd67a5b2d42cc89e3ce9846fa2cf2abe371902
Yer Thao <maivyerthao@gmail.com>
2021-02-05 19:26:59 -0600

    Updated table header color

--- src/components/AdminLandingPage/AdminLandingPage.js
+++ src/components/AdminLandingPage/AdminLandingPage.js
@@ -13,10 +13,13 @@ import Paper from '@material-ui/core/Paper';
 import Typography from "@material-ui/core/Typography";
 
 /*
-TO DO LIST ON THIS PAGE:
-  Map through the MATCHES & render onto the cards appropriately
-  onClick feature for Resource names: opens admin resource view/edit
-  onClick for Veteran names: opens admin veteran view
+----------------------------- TO DO LIST ON THIS PAGE: -----------------------------
+- Dispatch to saga to get detail of one specific vet by ID in URL
+- Receive details into a reducer
+- dispatch to call reducer to adminVetView and adminResourceEdit to 
+  see specific veteran and or resource profile information
+
+-------------------------------------------------------------------------------------
 */
 
 const styles = theme => ({
@@ -40,16 +43,16 @@ class AdminLandingPage extends Component {
     this.props.dispatch({ type: "FETCH_VET" });
   }
 
-  handleVeteran = (id) => {
-    console.log("VETERAN YOU SELECTED:", id);
-    // this.props.dispatch({type: ''})
+  handleVeteran = (veteranID) => {
+    console.log("VETERAN YOU SELECTED:", veteranID);
+      this.props.dispatch({type:'GET_ONE_VET', payload: veteranID});
     this.props.history.push("/adminVetView");
   };
 
   //TODO Dispatch RESOURCE YOU SELECTED
-  handleResource = (id) => {
-    console.log("RESOURCE YOU SELECTED:", id);
-    // this.props.dispatch({type: ''})
+  handleResource = (resourceID) => {
+    console.log("RESOURCE YOU SELECTED:", resourceID);
+    // this.props.dispatch({type: 'GET_RESOURCE', payload: resourceID})
     this.props.history.push("/adminResourceEdit");
   };
 
@@ -60,12 +63,12 @@ class AdminLandingPage extends Component {
       return (
         <div className="container">
         <center>
-        {JSON.stringify(this.props.store.vetReducer)}
+        {/* {JSON.stringify(this.props.store.vetReducer)} */}
         <h2>Admin Landing Page</h2>
         </center>
             <Paper className={classes.root}>
               <Table className={classes.table}>
-                <TableHead>
+                <TableHead className="table-head-color">
                   <TableRow>
                     <TableCell>Name</TableCell>
                     <TableCell align="right">Resource</TableCell>
