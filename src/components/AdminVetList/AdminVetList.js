658e8abe2eb9362b1cb5655f776a36a36e9eaa81
Cassen Gerber <cassenpt@gmail.com>
2021-02-06 12:48:38 -0600

    initial commit, installed nodemailer

--- src/components/AdminVetList/AdminVetList.js
+++ src/components/AdminVetList/AdminVetList.js
@@ -7,7 +7,6 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-// import classNames from 'classnames';
 import TextField from '@material-ui/core/TextField';
 
 const styles = {
@@ -40,6 +39,8 @@ const styles = {
     borderRadius: "4px"
   }
 };
+
+const transporter = nodemailer.createTransport(transport, [defaults])
 
 
 class AdminVetList extends Component {
