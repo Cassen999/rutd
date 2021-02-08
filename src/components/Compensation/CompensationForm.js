c8a548709760673fb3b84e49e343b598c66ca68a
joellekado <jkado@my.hpu.edu>
2021-02-08 15:43:12 -0600

    radion button labels

--- src/components/Compensation/CompensationForm.js
+++ src/components/Compensation/CompensationForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 //import { useDispatch } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import { connect } from 'react-redux';
-import { AppBar, FormControl, InputLabel, Select, Input, MenuItem, FormHelperText, Button, Toolbar, Grid, Badge, CardMedia, IconButton, makeStyles, Paper, InputBase, Card, CardActionArea } from '@material-ui/core'
+import { AppBar, FormControl, InputLabel, FormLabel, RadioGroup, FormControlLabel, Select, Input, MenuItem, FormHelperText, Button, Toolbar, Grid, Badge, CardMedia, IconButton, makeStyles, Paper, InputBase, Card, CardActionArea } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
@@ -53,9 +53,13 @@ componentDidMount(){
 
 }
 state = {
-   type: '0'
- };
+   type: '0',
+   value: '0' 
+};
 
+handleChange = event => {
+   this.setState({ value: event.target.value });
+ };
 
    handleInputChange = (event, inputProperty) => {
       console.log('Handling input-change...');
@@ -146,35 +150,55 @@ state = {
              <div> &nbsp;
 
 
-              { this.state.type === '1' && // if this part is false, the next part won't show
+               { this.state.type === '1' && // if this part is false, the next part won't show
                 
                 
-                <Radio
-                checked={this.state.type === '0'}
-                onChange={this.handleInputChangeFor('type')}
-                value='0'
-                name="No"
-                aria-label="No"
-                classes={{
-                  root: classes.root,
-                  checked: classes.checked,
-              }}
-             />
+                <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Gender</FormLabel>
+          <RadioGroup
+            aria-label="Gender"
+            name="gender1"
+            className={classes.group}
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            <FormControlLabel value="female" control={<Radio />} label="Female" />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <FormControlLabel
+              value="disabled"
+              disabled
+              control={<Radio />}
+              label="(Disabled option)"
+            />
+          </RadioGroup>
+        </FormControl>
                 
-                
-                } 
+                }  
          
-         <div>
-  {percentages.map((percent) => (
+       
+       
+       
+       
+         {/* <div>
+  {percentages.map((percent, i) => (
     this.state.type === '1'
 
       ? (              
 
       <Radio
-         checked={this.state.type === '0'}
+      key={i}
+         checked={this.setstate({
+            percentage : percent.id
+         })
+      
+      
+      
+      }
          onChange={this.handleInputChangeFor('type')}
-         value={percent}
+         value={percent.description}
          name="No"
+         label={percent.description}
          aria-label="No"
          classes={{
            root: classes.root,
@@ -183,113 +207,13 @@ state = {
       />)
       : null
   ))}
-</div>
+</div> */}
 
          
          
          
             </div>
 <form>
-{/* <FormControl className={classes.formControl}>
-<InputLabel htmlFor="age-auto-width">Age</InputLabel>
-           <Select
-            value={this.state.newVet.compensation}
-            // onChange={this.handleChange}
-            input={<Input name="age" id="age-label-placeholder" />}
-            displayEmpty
-            name="age"
-            className={classes.selectEmpty}
-          >
-             <MenuItem value="" disabled>
-              Placeholder
-            </MenuItem>
-           <MenuItem>
-         <em>No</em>
-           </MenuItem>
-           <MenuItem>
-          Yes
-           </MenuItem>
-</Select>
-<br/>
-<TextField
-                           id="standard-textarea"
-                           variant="outlined"
-                           label="Compensation"
-                           //  rows={4}
-                           //   rowsMax={20}
-                           name="compensation"
-                           //  multiline
-                           //  className={classes.inputs}
-                           //  style={{width: '130px', height: '130px', marginBottom: '50px'}}
-                           value={this.state.newVet.compensation}
-                           onChange={(event) => this.handleInputChange(event, 'compensation')}
-                        />
-                        <br/>
- <TextField
-                           id="standard-textarea"
-                           variant="outlined"
-                           label="Percentage"
-                           //  rows={4}
-                           //   rowsMax={20}
-                           name="percentage"
-                           //  multiline
-                           //  className={classes.inputs}
-                           //  style={{width: '130px', height: '130px', marginBottom: '50px'}}
-                           value={this.state.newVet.percentage}
-                           onChange={(event) => this.handleInputChange(event, 'percentage')}
-                        />
-                        <br/>
-
-                        <TextField
-                           id="standard-textarea"
-                           variant="outlined"
-                           label="Danger Areas"
-                           //  rows={4}
-                           //   rowsMax={20}
-                           name="danger_areas"
-                           //  multiline
-                           //  className={classes.inputs}
-                           //  style={{width: '130px', height: '130px', marginBottom: '50px'}}
-                           value={this.state.newVet.danger_areas}
-                           onChange={(event) => this.handleInputChange(event, 'danger_areas')}
-                        />
-                        <br/>
-
-                        <TextField
-                           id="standard-textarea"
-                           variant="outlined"
-                           label="Purple Heart"
-                           //  rows={4}
-                           //   rowsMax={20}
-                           name="purple_heart"
-                           //  multiline
-                           //  className={classes.inputs}
-                           //  style={{width: '130px', height: '130px', marginBottom: '50px'}}
-                           value={this.state.newVet.purple_heart}
-                           onChange={(event) => this.handleInputChange(event, 'purple_heart')}
-                        />
-                        <br/>
-                        <Button
-                           onClick={this.saveCompensation}
-                           // addArtProp={this.addArt}
-                           // elevation={20}
-                           //  className={classes.typography}
-                           //  textAlign='center'
-                           //  justify='center'
-                           // style={{ justifyContent: 'center' }}
-                        //  <Button elevation={10}
-                        //  Box display="flex" flexDirection="column"
-                        //  textAlign='center'
-                        //  display='flex'
-                        //  justifyContent='center'
-                        //  justify="center"
-                        //  onClick={this.addArt}
-                        //  variant="raised"
-                        //  color="purple"
-                        //  className={classes.alignAndJustify}
-                        >FINISH</Button>
-
-</FormControl> */}
    </form>
 
 
