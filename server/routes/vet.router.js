7c7ff1890c905280e90466c678c04cc418282494
Yer Thao <maivyerthao@gmail.com>
2021-02-08 11:23:27 -0600

    Updated get one veteran query in vet.router.js and p tags on AdminVetView

--- server/routes/vet.router.js
+++ server/routes/vet.router.js
@@ -126,22 +126,47 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
     let id = req.params.id;
   console.log('--- This is the ID of the veteran you clicked on: ',id);
   const queryText =
-    // `SELECT veteran.first_name, veteran.last_name, veteran.email, veteran.date_of_birth, veteran.number,
-    // veteran.children, veteran.homeless, veteran.current_address, veteran.city, veteran.city2, veteran.zipcode,
-    // veteran.mailing_address, veteran.zipcode2, veteran.start_date, veteran.end_date, veteran.compensation, veteran.danger_areas, veteran.purple_heart,
-    // gender.id, gender.description, married.id, married.description, state.id, 
-    // state.description, country.id, country.description, branch.id, branch.description,
-    // rank.id, rank.description, percentage.id, percentage.description
-    // FROM veteran
-    // JOIN gender ON gender.id = veteran.gender_id
-    // JOIN married ON married.id = veteran.married_id
-    // JOIN state ON state.id = veteran.state_id
-    // JOIN country ON country.id = veteran.country_id
-    // JOIN branch ON branch.id = veteran.branch_id
-    // JOIN rank ON rank.id = veteran.rank_id
-    // JOIN percentage ON percentage.id = veteran.percentage
-    // WHERE veteran.id = $1;`
-    `SELECT * FROM "veteran" where veteran.id = $1;`;
+    `SELECT 
+    veteran.first_name, 
+    veteran.last_name, 
+    veteran.email, 
+    veteran.date_of_birth, 
+    veteran.number,
+    veteran.children, 
+    veteran.homeless, 
+    veteran.current_address, 
+    veteran.city, 
+    veteran.city2, 
+    veteran.zipcode,
+    veteran.state_id2,
+    veteran.country_id2,
+    veteran.mailing_address, 
+    veteran.zipcode2, 
+    veteran.start_date, 
+    veteran.end_date, 
+    veteran.compensation, 
+    veteran.danger_areas, 
+    veteran.purple_heart,
+    injury.description AS injury,
+    discharge.description AS discharge,
+    gender.description AS gender,
+    married.description AS married ,
+    state.description AS state,
+    country.description AS country,
+    branch.description AS branch ,
+    rank.description AS rank ,
+    percentage.description AS percentage
+    FROM veteran
+    LEFT JOIN injury ON injury.id = veteran.injury_id
+    LEFT JOIN discharge ON discharge.id = veteran.discharge_id
+    LEFT JOIN gender ON gender.id = veteran.gender_id
+    LEFT JOIN married ON married.id = veteran.married_id
+    LEFT JOIN state ON state.id = veteran.state_id
+    LEFT JOIN country ON country.id = veteran.country_id
+    LEFT JOIN branch ON branch.id = veteran.branch_id
+    LEFT JOIN rank ON rank.id = veteran.rank_id
+    LEFT JOIN percentage ON percentage.id = veteran.percentage
+    WHERE veteran.id = $1;`;
   pool.query(queryText, [id])
     .then((result) => {
     console.log('GET This is the VETERAN you\'ve selected: ', result.rows); // WORKING
