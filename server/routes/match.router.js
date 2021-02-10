84d546c8aa1d1ac94b52c27db86e371edf877d03
Cassen Gerber <cassenpt@gmail.com>
2021-02-09 19:13:57 -0600

    put corret get back into match router

--- server/routes/match.router.js
+++ server/routes/match.router.js
@@ -29,32 +29,47 @@ router.get("/", rejectUnauthenticatedVet, (req, res) => {
     });
 });
 
-router.get("/complete/:id", rejectUnauthenticatedVet, (req, res) => {
-  // GET route for complete matches
-  console.log("in /match/complete GET route");
-  console.log("Is User logged in?");
-  console.log("req.user:", req.user);
+router.get('/complete/:id', (req, res) => {
+  // GET route for complete matches 
+  console.log('in /match/complete GET route');
+  console.log('Is User logged in?');
+  console.log('req.user:', req.user);
   let vetId = req.params.id;
-  let queryText = `SELECT * FROM (
-    SELECT oc.org_id,
-    count(oc.categories_id) AS org_needs,
-    count(vc.categories_id) AS vet_has,
-    (count(vc.categories_id) + 0.0) / (count(oc.categories_id) + 0.0) * 100 AS percent_match
-    FROM organization_categories oc
-    LEFT JOIN veteran_categories vc ON vc.categories_id = oc.categories_id
-    AND vc.vet_id = 1	
-    GROUP BY oc.org_id
-    ORDER BY percent_match desc, vet_has desc) AS records WHERE percent_match > 0;`;
-
-  pool
-    .query(queryText, [vetId])
-    .then((result) => {
+  let queryText = `SELECT match.*, veteran.first_name, organization.name, organization.number, organization.email, organization.website, organization.pictures, organization.description FROM match
+                  INNER JOIN veteran ON veteran.id = match.vet_id
+                  INNER JOIN organization ON organization.id = match.org_id
+                  WHERE veteran.id = $1;`;
+                    
+  pool.query(queryText, [vetId]).then((result) => {
       res.send(result.rows);
-    })
-    .catch((error) => {
+  }).catch((error) => {
       console.log(error);
       res.sendStatus(500);
-    });
+  });
 });
+
+// router.get("/complete/:id", rejectUnauthenticatedVet, (req, res) => {
+//   // GET route for complete matches
+//   let queryText = `SELECT * FROM (
+//     SELECT oc.org_id,
+//     count(oc.categories_id) AS org_needs,
+//     count(vc.categories_id) AS vet_has,
+//     (count(vc.categories_id) + 0.0) / (count(oc.categories_id) + 0.0) * 100 AS percent_match
+//     FROM organization_categories oc
+//     LEFT JOIN veteran_categories vc ON vc.categories_id = oc.categories_id
+//     AND vc.vet_id = 1	
+//     GROUP BY oc.org_id
+//     ORDER BY percent_match desc, vet_has desc) AS records WHERE percent_match > 0;`;
+
+//   pool
+//     .query(queryText)
+//     .then((result) => {
+//       res.send(result.rows);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.sendStatus(500);
+//     });
+// });
 
 module.exports = router;
