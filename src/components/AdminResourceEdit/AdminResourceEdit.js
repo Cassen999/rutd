e86b4b76c862ad586611397a6ccb41053008cdc3
Yer Thao <maivyerthao@gmail.com>
2021-02-10 10:18:30 -0600

    Added material-ui Paper styling to AdminResourceEdit

--- src/components/AdminResourceEdit/AdminResourceEdit.js
+++ src/components/AdminResourceEdit/AdminResourceEdit.js
@@ -4,6 +4,9 @@ import mapStoreToProps from "../../redux/mapStoreToProps";
 import "../AdminVetView/AdminVetView.css";
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 
 // TO DO LIST ON THIS PAGE:
 // - Make this page editable for admin with text fields
@@ -15,6 +18,11 @@ const styles = theme => ({
   },
   input: {
     display: 'none',
+  },
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
   },
 });
 
@@ -36,39 +44,67 @@ class AdminResourceEdit extends Component {
   render() {
     // line below is equivalent to this.props.store.details
     const { resourceDetails } = this.props.store;
-    return (
-      <div className="container">
-        <center>
-          {JSON.stringify(this.props.store.resourceDetails)}
-          <h2>{this.state.heading}</h2>
-        </center>
-        {resourceDetails.map((resource, i) => {
+        const { classes } = this.props;
           return (
-            <div className="flex-grid" key={i}>
-              <div className="col">
-                <h3>Resource Name: {resource.name}</h3>
-                <p>Number: {resource.number}</p>
-                <p>Email: {resource.email}</p>
-                <p>City: {resource.city}</p>
-                <p>State: {resource.state}</p>
-                <p>Website:{ resource.website}</p>
-                <p>Description: {resource.description}</p>
-                <p>Categories: {resource.categories}</p>
-                {/* <p>PDF:{resource.pdf}</p> */}
-                {/* <p>Pictures: {resource.pictures}</p> */}
-              </div>
+            <div className="container">
+              <center>
+                {JSON.stringify(this.props.store.resourceDetails)}
+                <h2>{this.state.heading}</h2>
+              </center>
+              {resourceDetails.map((resource, i) => {
+                return (
+                  <div key={i}>
+                    <Paper className={classes.root} elevation={1}>
+                      <Typography variant="h5" component="h3">
+                        Organization Details
+                      </Typography>
+                      <Typography component="p">
+                        Name: {resource.name}
+                      </Typography>
+                      <Typography component="p">
+                        Number: {resource.number}
+                      </Typography>
+                      <Typography component="p">
+                        Email: {resource.email}
+                      </Typography>
+                      <Typography component="p">
+                        City: {resource.city}
+                      </Typography>
+                      <Typography component="p">
+                        State: {resource.state}
+                      </Typography>
+                      <Typography component="p">
+                        Website:{ resource.website}
+                      </Typography>
+                      <Typography component="p">
+                        Description: {resource.description}
+                      </Typography>
+                      <Typography component="p">
+                        Categories: {resource.categories}
+                      </Typography>
+                      <Typography component="p">
+                        PLACEHOLDER FOR PDF
+                      </Typography>
+                      <Typography component="p">
+                        PLACEHOLDER FOR PICTURES
+                      </Typography>
+                    </Paper>
+                  </div>
+                );
+              })}
+              <hr></hr>
+                <center>
+                  <Button  
+                    className="admin-landing-button" variant="contained" 
+                    onClick={this.goBackHome}>Admin Landing Page</Button>
+                    <span>&nbsp;&nbsp;</span>
+                  <Button  
+                    className="admin-landing-button" variant="contained" 
+                    onClick={this.editOrganization}>Edit</Button>
+                </center>
             </div>
           );
-        })}
-        <hr></hr>
-          <center>
-            <Button  
-              className="admin-landing-button" variant="contained" 
-              onClick={this.goBackHome}>Admin Landing Page</Button>
-          </center>
-      </div>
-    );
   }
 }
 
-export default connect(mapStoreToProps)(AdminResourceEdit);
+export default connect(mapStoreToProps)(withStyles(styles)(AdminResourceEdit));
