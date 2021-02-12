2287d9c457084ff4c3dffe9388f18ee214bf0655
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2021-02-11 18:18:50 -0600

    working on the put

--- server/routes/resource.router.js
+++ server/routes/resource.router.js
@@ -32,7 +32,8 @@ router.post("/", rejectUnauthenticatedAdmin, (req, res) => {
                       (name, number, email, city, state_id, pdf, website, 
                       pictures, description, categories_id, approved)	
                       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, 
-                      $11) RETURNING "id";`;
+                      $11)  RETURNING "id";`;
+
   pool
     .query(queryText, [
       name,
@@ -54,6 +55,60 @@ router.post("/", rejectUnauthenticatedAdmin, (req, res) => {
     });
 });
 
+// PUT route for org
+
+router.put("/id", rejectUnauthenticatedAdmin, (req, res) => {
+  console.log("Updating Org ", req.body); // not coming through
+  let orgID = req.params.id;
+  // let userID = req.user.id;
+  console.log("ROUTER PUT: ID of the dream you are updating: ", orgID);
+  console.log("ROUTER PUT: user ID:", userID);
+  // capture
+  const name = req.body.name;
+  const number = req.body.number;
+  const email = req.body.email;
+  const city = req.body.city;
+  const state_id = req.body.state_id;
+  const pdf = req.body.pdf;
+  const website = req.body.website;
+  const pictures = req.body.pictures;
+  const description = req.body.description;
+  const categories_id = req.body.categories_id;
+  const approved = req.body.approved;
+  const queryText = `UPDATE organization SET
+    name=$1, number=$2, email=$3, city=$4, state_id=$5, pdf=$6,
+    website=$7, pictures=$8, description=$9, categories_id=$10, approved=$11 WHERE id= $12;`;
+  pool
+    .query(queryText, [
+      name,
+      number,
+      email,
+      city,
+      state_id,
+      pdf,
+      website,
+      pictures,
+      description,
+      categories_id,
+      approved,
+    ])
+    .then((data) => {
+      console.log(data);
+      const queryText =
+        "SELECT * FROM organization LEFT JOIN (SELECT model.id, model.name as model, make.name as make from model left join make on model.make_id = make.id) as model ON vehicle.model_id = model.id;";
+      pool
+        .query(queryText)
+        .then((data) => res.json(data.rows))
+        .catch((err) => {
+          console.log("Get Vehicles failed: ", err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      console.log("Update Vehicles failed: ", err);
+      res.sendStatus(500);
+    });
+});
 // GETS one specific resource
 router.get("/:id", rejectUnauthenticatedAdmin, (req, res) => {
   let id = req.params.id;
