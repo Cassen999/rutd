682b1e81cbf7fa35203352c461a5ec6625496803
Yer Thao <maivyerthao@gmail.com>
2021-02-15 11:38:42 -0600

    Service history questions onto DOM

--- src/components/ServiceHistoryQuestions/ServiceHistoryQuestions.js
+++ src/components/ServiceHistoryQuestions/ServiceHistoryQuestions.js
@@ -4,7 +4,22 @@ class ServiceHistoryQuestions extends Component {
     state = {  }
     render() { 
         return ( 
-            <h1>Service History</h1>
+            <div className="container">
+                <h1>Service History</h1>
+                <p>SERVICE STATUS DROPDOWN HERE: Active/Veteran</p>
+                <br></br>
+                <p>TYPE OF DISCHARGE DROPDOWN HERE: Honorable/Other</p>
+                <br></br>
+                <p>BRANCH OF SERVICE DROPDOWN HERE: Army/Marines, etc.</p>
+                <br></br>
+                <p>HIGHEST ATTAINED RANK DROPDOWN HERE: Enlisted/Officer, etc</p>
+                <br></br>
+                <p>SERVICE END DATE: ADD A CALENDAR HERE?</p>
+                <br></br>
+                <p>Are you currently serving or served in the military on or after September 11, 2001?</p>
+                <p>RADIO BUTTONS HERE</p>
+
+            </div>
          );
     }
 }
