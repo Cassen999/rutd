b7d082904d4433db16d04bbf2cf49a8b11d5ea85
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2021-02-18 11:15:39 -0600

    clean warnings

--- src/components/MiscQuestions/MiscQuestions.js
+++ src/components/MiscQuestions/MiscQuestions.js
@@ -4,170 +4,177 @@ import Hazard from "./Miscellaneous/Hazard";
 import PurpleHeart from "./Miscellaneous/PurpleHeart";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import { connect } from "react-redux";
-import { withStyles, Typography, Fab } from '@material-ui/core';
-import SaveTwoToneIcon from '@material-ui/icons/SaveTwoTone';
+import { withStyles, Fab } from "@material-ui/core";
+import SaveTwoToneIcon from "@material-ui/icons/SaveTwoTone";
 
 const styles = (theme) => ({
-    formControl: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      minWidth: 185, 
+  formControl: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    minWidth: 185,
+  },
+  formColor: {
+    textAlign: "left",
+    marginBottom: "10px",
+  },
+  selectContainer: {
+    textAlign: "left",
+    padding: theme.spacing(1),
+  },
+  formTitle: {
+    color: "black",
+    textAlign: "center",
+    borderBottom: "2px solid grey",
+    borderTop: "2px solid grey",
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(2),
+    backgroundColor: "#AFFA3D",
+  },
+  textControl: {
+    color: "grey",
+    textAlign: "center",
+    borderBottom: "2px solid grey",
+    borderTop: "2px solid grey",
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(3),
+    marginTop: theme.spacing(2),
+    width: "80%",
+    display: "inline-flex",
+  },
+  lineStyle: {
+    width: "90%",
+    color: "grey",
+    backgroundColor: "white",
+    height: 6,
+    marginTop: 0,
+    marginBottom: 2,
+  },
+  root: {
+    "&:hover": {
+      backgroundColor: "transparent",
     },
-    formColor: {
-        textAlign: 'left',
-        marginBottom: '10px'
+  },
+  icon: {
+    borderRadius: "50%",
+    width: 20,
+    height: 20,
+    boxShadow:
+      "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
+    backgroundColor: "#f5f8fa",
+    "input:hover ~ &": {
+      backgroundColor: "#ebf1f5",
     },
-    selectContainer: {
-        textAlign: 'left',
-        padding: theme.spacing(1)
+    "input:disabled ~ &": {
+      boxShadow: "none",
+      background: "rgba(206,217,224,.5)",
     },
-    formTitle: {
-        color: 'black',
-        textAlign: 'center',
-        borderBottom: '2px solid grey',
-        borderTop: '2px solid grey',
-        padding: theme.spacing(1),
-        marginBottom: theme.spacing(4),
-        marginTop: theme.spacing(2),
-        backgroundColor: '#AFFA3D'
-    },
-    textControl: {
-        color: 'grey',
-        textAlign: 'center',
-        borderBottom: '2px solid grey',
-        borderTop: '2px solid grey',
-        padding: theme.spacing(1),
-        marginBottom: theme.spacing(3),
-        marginTop: theme.spacing(2),
-        width: "80%",
-        display: 'inline-flex',
-    },
-    lineStyle: { 
-        width: "90%",
-        color: 'grey',
-        backgroundColor: 'white',
-        height: 6,
-        marginTop: 0,
-        marginBottom: 2
-    },
-    root: {
-      '&:hover': {
-        backgroundColor: 'transparent',
-      },
-    },
-    icon: {
-      borderRadius: '50%',
+  },
+  checkedIcon: {
+    backgroundColor: "#5b6357",
+    backgroundImage:
+      "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
+    "&:before": {
+      display: "block",
       width: 20,
       height: 20,
-      boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-      backgroundColor: '#f5f8fa',
-      'input:hover ~ &': {
-        backgroundColor: '#ebf1f5',
-      },
-      'input:disabled ~ &': {
-        boxShadow: 'none',
-        background: 'rgba(206,217,224,.5)',
-      },
+      backgroundImage: "radial-gradient(#fff,#fff 28%,transparent 32%)",
+      content: '""',
     },
-    checkedIcon: {
-      backgroundColor: '#5b6357',
-      backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-      '&:before': {
-        display: 'block',
-        width: 20,
-        height: 20,
-        backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
-        content: '""',
-      },
-      'input:hover ~ &': {
-        backgroundColor: '#727372',
-      },
+    "input:hover ~ &": {
+      backgroundColor: "#727372",
     },
-  });
+  },
+});
 
-
-// This component displays the intake questionaire for the 'Miscellaneous' tab \\   
+// This component displays the intake questionaire for the 'Miscellaneous' tab \\
 class MiscQuestions extends Component {
-    state = {
-        userId: this.props.store.user.id, 
-        compensationId: 0,
-        registered: '',
-        imminentDanger: '',
-        dangerDescription: '',
-        purpleHeart: '',
-    }
+  state = {
+    userId: this.props.store.user.id,
+    compensationId: 0,
+    registered: "",
+    imminentDanger: "",
+    dangerDescription: "",
+    purpleHeart: "",
+  };
 
-    //converts string booleans to boolean values and updates state with them
-    updateState = (event, propertyName) => {
-        let value = event.target.value
-        if (value === 'false'){
-            value = false;
-        } else if (value === 'true'){
-            value = true;
-        }
-        this.setState({
-            [propertyName]: value
-        }) 
+  //converts string booleans to boolean values and updates state with them
+  updateState = (event, propertyName) => {
+    let value = event.target.value;
+    if (value === "false") {
+      value = false;
+    } else if (value === "true") {
+      value = true;
     }
+    this.setState({
+      [propertyName]: value,
+    });
+  };
 
-    saveProgress = (event) => {
-        event.preventDefault();
-        console.log('Saving Progress', this.state);
-        this.props.dispatch({
-            type: 'UPDATE_MISC_QUESTIONS',
-            payload: this.state
-        })
-    }
+  saveProgress = (event) => {
+    event.preventDefault();
+    console.log("Saving Progress", this.state);
+    this.props.dispatch({
+      type: "UPDATE_MISC_QUESTIONS",
+      payload: this.state,
+    });
+  };
 
-    render() {
-        const { classes } = this.props;
-        const { compensationId, registered, imminentDanger, dangerDescription, purpleHeart } = this.state;
-        return ( 
-            <div 
-                className="container"
-                display="inline-flex"
-            >
-                <h1 className="grey">Miscellaneous</h1>
-                <hr className="float-left no-margin hr-width"></hr>
-                <br></br>
-                <form className={classes.formColor}>
-                    <Compensation
-                        registered={registered}
-                        compensationId={compensationId}
-                        updateState={this.updateState}
-                        classes={classes} 
-                    />
-                    {/* ---------------------------------- */}
-                    <Hazard
-                        imminentDanger={imminentDanger}
-                        dangerDescription={dangerDescription}
-                        updateState={this.updateState}
-                        classes={classes} 
-                    />
-                    {/* ---------------------------------- */}
-                    <PurpleHeart
-                        purpleHeart={purpleHeart}
-                        updateState={this.updateState}
-                        classes={classes}
-                        saveProgress={this.saveProgress}
-                    />
-                    {/* ---------------------------------- */}
-                    <Fab
-                        className="float-right"
-                        style={{
-                            borderRadius: 35,
-                            backgroundColor: '#AFFA3D',
-                            fontFamily: 'orbitron',
-                            marginBottom: '10%',
-                            marginRight: '10px',
-                        }}
-                        onClick={(event) => { this.saveDemographic(event) }}>
-                            <SaveTwoToneIcon />
-                    </Fab>
-                </form>
-            </div>
-         );
-    }
+  render() {
+    const { classes } = this.props;
+    const {
+      compensationId,
+      registered,
+      imminentDanger,
+      dangerDescription,
+      purpleHeart,
+    } = this.state;
+    return (
+      <div className="container" display="inline-flex">
+        <h1 className="grey">Miscellaneous</h1>
+        <hr className="float-left no-margin hr-width"></hr>
+        <br></br>
+        <form className={classes.formColor}>
+          <Compensation
+            registered={registered}
+            compensationId={compensationId}
+            updateState={this.updateState}
+            classes={classes}
+          />
+          {/* ---------------------------------- */}
+          <Hazard
+            imminentDanger={imminentDanger}
+            dangerDescription={dangerDescription}
+            updateState={this.updateState}
+            classes={classes}
+          />
+          {/* ---------------------------------- */}
+          <PurpleHeart
+            purpleHeart={purpleHeart}
+            updateState={this.updateState}
+            classes={classes}
+            saveProgress={this.saveProgress}
+          />
+          {/* ---------------------------------- */}
+          <Fab
+            className="float-right"
+            style={{
+              borderRadius: 35,
+              backgroundColor: "#AFFA3D",
+              fontFamily: "orbitron",
+              marginBottom: "10%",
+              marginRight: "10px",
+            }}
+            onClick={(event) => {
+              this.saveDemographic(event);
+            }}
+          >
+            <SaveTwoToneIcon />
+          </Fab>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(withStyles(styles)(MiscQuestions));
--- src/components/MiscQuestions/Miscellaneous/Compensation.js
+++ src/components/MiscQuestions/Miscellaneous/Compensation.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import mapStoreToProps from "../../../redux/mapStoreToProps";
 import { connect } from "react-redux";
-import clsx from 'clsx';
-import {  
+import clsx from "clsx";
+import {
   InputLabel,
   MenuItem,
   FormControl,
   Select,
   Radio,
-  Grid,
-  Typography 
-} from "@material-ui/core";                
+  Typography,
+} from "@material-ui/core";
 class Compensation extends Component {
-
   state = {
     open: false,
   };
@@ -35,65 +33,63 @@ class Compensation extends Component {
     const open = this.state.open;
     const registered = this.props.registered;
     const compensationId = this.props.compensationId;
-    return (   
+    return (
       <div display="inline-flex">
         <Typography className={classes.textControl}>
           Are you currently registered with the VA?
         </Typography>
-          <div 
-            className={classes.selectContainer}
-            display="flex"  
-          >
-            <label className={classes.label}>Yes</label>
-            <Radio
-              className={classes.root}
-              disableRipple
-              color="default"
-              checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
-              icon={<span className={classes.icon} />}      
-              checked={registered === true}
-              value={true}
-              onClick={(event) => this.props.updateState(event, 'registered')}
-            />
-            <label className={classes.label}>No</label>         
-            <Radio
-              className={classes.root}
-              disableRipple
-              color="default"
-              checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
-              icon={<span className={classes.icon} />}
-              checked={registered === false} 
-              value={false}
-              onClick={(event) => this.props.updateState(event, 'registered')}
-            />
-              {registered === true && (
-                <FormControl variant="filled" className={classes.formControl}>
-                  <InputLabel id="open-comp-label">Compensation Rate</InputLabel>
-                  <Select
-                    labelId="open-comp-label"
-                    id="open-comp"
-                    open={open}
-                    onClose={this.handleClose}
-                    onOpen={this.handleOpen}
-                    value={compensationId}
-                    onChange={(event) => this.props.updateState(event, 'compensationId')}
-                  >
-                    {percentages.map((percent) => (
-                      <MenuItem
-                        key={percent.description}
-                        value={percent.id}
-                      >
-                        {percent.description}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>     
-              )}
-          </div>
+        <div className={classes.selectContainer} display="flex">
+          <label className={classes.label}>Yes</label>
+          <Radio
+            className={classes.root}
+            disableRipple
+            color="default"
+            checkedIcon={
+              <span className={clsx(classes.icon, classes.checkedIcon)} />
+            }
+            icon={<span className={classes.icon} />}
+            checked={registered === true}
+            value={true}
+            onClick={(event) => this.props.updateState(event, "registered")}
+          />
+          <label className={classes.label}>No</label>
+          <Radio
+            className={classes.root}
+            disableRipple
+            color="default"
+            checkedIcon={
+              <span className={clsx(classes.icon, classes.checkedIcon)} />
+            }
+            icon={<span className={classes.icon} />}
+            checked={registered === false}
+            value={false}
+            onClick={(event) => this.props.updateState(event, "registered")}
+          />
+          {registered === true && (
+            <FormControl variant="filled" className={classes.formControl}>
+              <InputLabel id="open-comp-label">Compensation Rate</InputLabel>
+              <Select
+                labelId="open-comp-label"
+                id="open-comp"
+                open={open}
+                onClose={this.handleClose}
+                onOpen={this.handleOpen}
+                value={compensationId}
+                onChange={(event) =>
+                  this.props.updateState(event, "compensationId")
+                }
+              >
+                {percentages.map((percent) => (
+                  <MenuItem key={percent.description} value={percent.id}>
+                    {percent.description}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+        </div>
       </div>
-    ); 
+    );
   }
-} 
+}
 export default connect(mapStoreToProps)(Compensation);
-  
-
