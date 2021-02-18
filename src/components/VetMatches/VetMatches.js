bc2599eae87b131e58c62cc5b49c13f90ab10eb0
Cassen Gerber <cassenpt@gmail.com>
2021-02-18 15:04:08 -0600

    fixed glitched conflict

--- src/components/VetMatches/VetMatches.js
+++ src/components/VetMatches/VetMatches.js
@@ -3,20 +3,6 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import { withRouter } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
-<<<<<<< HEAD
-import Button from '@material-ui/core/Button';
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import swal from 'sweetalert';
-import RedCross from '../../Images/redcross.jpg';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
-import CardHeader from '@material-ui/core/CardHeader';
-=======
-// import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import swal from "sweetalert";
@@ -28,7 +14,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Avatar from "@material-ui/core/Avatar";
 import CardHeader from "@material-ui/core/CardHeader";
->>>>>>> master
 import Typography from "@material-ui/core/Typography";
 
 const styles = (theme) => ({
