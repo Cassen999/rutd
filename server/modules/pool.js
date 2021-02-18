c3c99d667fb49b405d9582038d7a19d82035f3b2
Cassen Gerber <cassenpt@gmail.com>
2021-02-18 11:12:53 -0600

    changed database line in pool.js to database: process.env.DATABASE_NAME || rutd

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,18 +1,9 @@
-/* the only line you likely need to change is
-
- database: 'prime_app',
-
- change `prime_app` to the name of your database, and you should be all set!
-*/
-
 const pg = require('pg');
 const url = require('url');
 
 let config = {};
 
 if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
-  // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth.split(':');
 
@@ -21,26 +12,23 @@ if (process.env.DATABASE_URL) {
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
+    database: process.env.DATABASE_NAME || 'rutd',
     ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    max: 10, 
+    idleTimeoutMillis: 30000,
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'rutd', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    host: 'localhost', 
+    port: 5432, 
+    database: 'rutd', 
+    max: 10,
+    idleTimeoutMillis: 30000,
   };
 }
 
-// this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
 
-// the pool with emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
   process.exit(-1);
